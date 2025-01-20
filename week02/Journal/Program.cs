676cacc1fd3a25ef0caa6589ea4e0681b0777891
Lehi Gomez <gom23011@byui.edu>
2025-01-20 18:04:24 +0800

    Abstraction Practice Complete

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,5 +1,10 @@
 using System;
 
+public class Journal
+{
+    // Add properties and methods for the Journal class here
+}
+
 class Program
 {
     static void Main(string[] args)
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        var job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        var job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Microsoft";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        var resume = new Resume();
+        resume._name = "Lehi Gomez";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        
+        resume.Display();
+
     }
 }
