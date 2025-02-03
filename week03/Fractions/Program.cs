791083be246e85265be49c130dcd11628bbd5941
Lehi Gomez <gom23011@byui.edu>
2025-02-03 15:36:39 +0800

    New structure

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,70 +1,22 @@
 using System;
 
-public class Fraction
+class Program
 {
-    private int _top = 0;
-    private int _bottom = 0;
-
-    public Fraction()
-    {
-        _top = 1;
-        _bottom = 1;
-    }
-    public Fraction(int wholeNumber)
-    {
-        _top = wholeNumber;
-        _bottom = 1;
-    }
-    public Fraction(int top, int bottom)
-    {
-        _top = top;
-        _bottom = bottom;
-    }
-
-    public int GetTop()
-    {
-        return _top;
-    }
-    public void SetTop(int top)
-    {
-        _top = top;
-    }
-    public int GetBottom()
-    {
-        return _bottom;
-    }
-    public void SetBottom(int bottom)
-    {
-        _bottom = bottom;
-    }
-    
-    public string GetFractionString()
-    {
-        return $"{_top}/{_bottom}";
-    }
-    public double GetDecimalValue()
-    {
-        return (double)_top / _bottom;
-    }
-
-}
-
-public class Program
-{
-    public static void Main()
+    static void Main()
     {
         Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(5);
-        Fraction fraction3 = new Fraction(3, 4);
-
         Console.WriteLine(fraction1.GetFractionString());
         Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
         Console.WriteLine(fraction2.GetFractionString());
         Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
         Console.WriteLine(fraction3.GetFractionString());
         Console.WriteLine(fraction3.GetDecimalValue());
-        fraction3.SetTop(1);
-        fraction3.SetBottom(3);
+
+        Fraction fraction4 = new Fraction(1, 3);
         Console.WriteLine(fraction3.GetFractionString());
         Console.WriteLine(fraction3.GetDecimalValue());
 
